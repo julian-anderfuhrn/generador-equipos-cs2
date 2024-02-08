e2617e42c2ab305efdb2182910235582fbc480cc
HoudiniDev <74371270+julian-anderfuhrn@users.noreply.github.com>
2024-02-08 12:15:10 -0300

    fix title

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,10 +4,10 @@ export default function Title() {
   return (
     <Box sx={{ padding: 3 }}>
       <Typography variant="h4" sx={{ textAlign: "center" }}>
-        Armador de equipos
+        Generador de equipos
       </Typography>
       <Typography variant="subtitle1" sx={{ textAlign: "center" }}>
-        Armador de 2 equipos, emparejando segun el nivel de cada jugador
+        Generador de 2 equipos, emparejando segun el nivel de cada jugador
       </Typography>
       <Typography variant="body2" sx={{ mt: 2 }}>
         <b>*El numero de jugadores debe ser par</b>
